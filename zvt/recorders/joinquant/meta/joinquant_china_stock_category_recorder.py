9055d58abc2513cf5fc4c018754cd460053c1720
wuhgogo <wuhgogo@gmail.com>
2021-01-14 01:19:50 +0800

    almost finished

--- zvt/recorders/joinquant/meta/joinquant_china_stock_category_recorder.py
+++ zvt/recorders/joinquant/meta/joinquant_china_stock_category_recorder.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
-import json
 
-import demjson
 import pandas as pd
-import requests
+from jqdatapy.api import request_jqdata
 
 from zvt.contract.api import df_to_db
 from zvt.contract.recorder import Recorder, TimeSeriesDataRecorder
+from zvt.domain import BlockStock, Block, Stock
+from zvt.recorders.joinquant.common import to_entity_id
 from zvt.utils.time_utils import now_pd_timestamp
-from zvt.api.quote import china_stock_code_to_id
-from zvt.domain import BlockStock, BlockCategory, Block
-from jqdatapy.api import request_jqdata
 
 
 class JoinquantChinaBlockRecorder(Recorder):
@@ -19,74 +16,30 @@ class JoinquantChinaBlockRecorder(Recorder):
 
     def run(self):
         # get stock blocks from joinquant
-        df_block = request_jqdata('get_concepts')
-        df =
+        category = 'industry'
+
+        df_block = request_jqdata('get_concepts', parse_dates=['start_date'])
+        df_block['entity_id'] = df_block['code'].apply(lambda x: f'block_cn_{x}')
+        df_block['id'] = df_block['entity_id']
+        df_block.rename(columns={'start_date': 'timestamp'}, inplace=True)
+        df_block['timestamp'] = pd.to_datetime(df_block['timestamp'])
+        df_block['entity_type'] = 'block'
+        df_block['exchange'] = 'cn'
+        df_block['list_date'] = df_block['timestamp']
+        df_block['category'] = category
+
+        df_to_db(data_schema=self.data_schema, df=df_block, provider=self.provider,
+                 force_update=True)
+
+        self.logger.info(f"finish record joinquant blocks:{category}")
 
 
-
-        df = df_block.set_index('code')
-        df.index.name = 'entity_id'
-        df = df.reset_index()
-        # 上市日期
-        df.rename(columns={'start_date': 'timestamp'}, inplace=True)
-        df['timestamp'] = pd.to_datetime(df['timestamp'])
-        df['list_date'] = df['timestamp']
-        df['end_date'] = pd.to_datetime(df['end_date'])
-
-        df['entity_id'] = df['entity_id'].apply(lambda x: to_entity_id(entity_type=entity_type, jq_code=x))
-        df['id'] = df['entity_id']
-        df['entity_type'] = entity_type
-        df['exchange'] = df['entity_id'].apply(lambda x: get_entity_exchange(x))
-        df['code'] = df['entity_id'].apply(lambda x: get_entity_code(x))
-        df['name'] = df['display_name']
-
-        if category:
-            df['category'] = category
-        df_to_db(df_stock, data_schema=Stock, provider=self.provider, force_update=self.force_update)
-        # persist StockDetail too
-        df_to_db(df=df_stock, data_schema=StockDetail, provider=self.provider, force_update=self.force_update)
-
-        # self.logger.info(df_stock)
-        self.logger.info("persist stock list success")
-        for category, url in self.category_map_url.items():
-            resp = requests.get(url)
-            resp.encoding = 'GBK'
-
-            tmp_str = resp.text
-            json_str = tmp_str[tmp_str.index('{'):tmp_str.index('}') + 1]
-            tmp_json = json.loads(json_str)
-
-            the_list = []
-
-            for code in tmp_json:
-                name = tmp_json[code].split(',')[1]
-                entity_id = f'block_cn_{code}'
-                the_list.append({
-                    'id': entity_id,
-                    'entity_id': entity_id,
-                    'entity_type': 'block',
-                    'exchange': 'cn',
-                    'code': code,
-                    'name': name,
-                    'category': category.value
-                })
-            if the_list:
-                df = pd.DataFrame.from_records(the_list)
-                df_to_db(data_schema=self.data_schema, df=df, provider=self.provider,
-                         force_update=True)
-
-            self.logger.info(f"finish record sina blocks:{category.value}")
-
-
-class SinaChinaBlockStockRecorder(TimeSeriesDataRecorder):
-    entity_provider = 'sina'
+class JoinquantChinaBlockStockRecorder(TimeSeriesDataRecorder):
+    entity_provider = 'joinquant'
     entity_schema = Block
 
-    provider = 'sina'
+    provider = 'joinquant'
     data_schema = BlockStock
-
-    # 用于抓取行业包含的股票
-    category_stocks_url = 'http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page={}&num=5000&sort=symbol&asc=1&node={}&symbol=&_s_r_a=page'
 
     def __init__(self, entity_type='block', exchanges=None, entity_ids=None, codes=None, batch_size=10,
                  force_update=True, sleeping_time=5, default_size=2000, real_time=False, fix_duplicate_way='add',
@@ -96,46 +49,39 @@ class SinaChinaBlockStockRecorder(TimeSeriesDataRecorder):
                          close_minute)
 
     def record(self, entity, start, end, size, timestamps):
-        for page in range(1, 5):
-            resp = requests.get(self.category_stocks_url.format(page, entity.code))
-            try:
-                if resp.text == 'null' or resp.text is None:
-                    break
-                category_jsons = demjson.decode(resp.text)
-                the_list = []
-                for category in category_jsons:
-                    stock_code = category['code']
-                    stock_id = china_stock_code_to_id(stock_code)
-                    block_id = entity.id
-                    the_list.append({
-                        'id': '{}_{}'.format(block_id, stock_id),
-                        'entity_id': block_id,
-                        'entity_type': 'block',
-                        'exchange': entity.exchange,
-                        'code': entity.code,
-                        'name': entity.name,
-                        'timestamp': now_pd_timestamp(),
-                        'stock_id': stock_id,
-                        'stock_code': stock_code,
-                        'stock_name': category['name'],
+        df_block_stock = request_jqdata('get_concept_stocks', concept_code=entity.code)
+        the_list = []
+        for _, stock in df_block_stock.iterrows():
+            stock_id = to_entity_id(stock, 'stock')
+            stock_entity = Stock.query_data(provider=self.provider, entity_id=stock_id)
+            stock_code = stock_entity['code']
+            block_id = entity.id
+            the_list.append({
+                'id': '{}_{}'.format(block_id, stock_id),
+                'entity_id': block_id,
+                'entity_type': 'block',
+                'exchange': entity.exchange,
+                'code': entity.code,
+                'name': entity.name,
+                'timestamp': now_pd_timestamp(),
+                'stock_id': stock_id,
+                'stock_code': stock_code,
+                'stock_name': stock_entity['name']
+            })
+            if the_list:
+                df = pd.DataFrame.from_records(the_list)
+                df_to_db(data_schema=self.data_schema, df=df, provider=self.provider,
+                         force_update=True)
 
-                    })
-                if the_list:
-                    df = pd.DataFrame.from_records(the_list)
-                    df_to_db(data_schema=self.data_schema, df=df, provider=self.provider,
-                             force_update=True)
-
-                self.logger.info('finish recording BlockStock:{},{}'.format(entity.category, entity.name))
-
-            except Exception as e:
-                self.logger.error("error:,resp.text:", e, resp.text)
-            self.sleep()
+        self.logger.info('finish recording joinquant BlockStock:{},{}'.format(entity.category, entity.name))
 
 
-__all__ = ['SinaChinaBlockRecorder', 'SinaChinaBlockStockRecorder']
+__all__ = ['JoinquantChinaBlockRecorder', 'JoinquantChinaBlockStockRecorder']
 
 if __name__ == '__main__':
     # init_log('sina_china_stock_category.log')
 
-    recorder = SinaChinaBlockStockRecorder(codes=['new_cbzz'])
-    recorder.run()
+    # recorder = JoinquantChinaBlockRecorder(codes=['new_cbzz'])
+    # recorder.run()
+    # JoinquantChinaBlockRecorder().run()
+    JoinquantChinaBlockStockRecorder(codes=['GN705']).run()
